1a7a2768f2e234dbd8fed5fb673ce81153075c66
Joao Zanon <jot@jot.com.br>
2025-03-06 14:11:43 -0300

    feat: adiciona fu00e1bricas para criau00e7u00e3o de componentes

---
+++ src/Factories/QueryBuilderFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jot\HfElastic\Factories;
+
+use Jot\HfElastic\Contracts\QueryBuilderInterface;
+
+/**
+ * Factory for creating query builder instances.
+ */
+class QueryBuilderFactory
+{
+    /**
+     * Creates a new query builder instance.
+     *
+     * @return QueryBuilderInterface A new query builder instance.
+     */
+    public function create(): QueryBuilderInterface
+    {
+        return make(QueryBuilderInterface::class);
+    }
+}
