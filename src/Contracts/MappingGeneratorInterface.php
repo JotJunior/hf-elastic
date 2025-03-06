062d9d709af8b49fc0a1a266d2038abdb26a4d12
Joao Zanon <jot@jot.com.br>
2025-03-06 17:19:47 -0300

    feat: adiciona interfaces para Mapping, Property e MappingGenerator

---
+++ src/Contracts/MappingGeneratorInterface.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Jot\HfElastic\Contracts;
+
+/**
+ * Interface for classes that generate Elasticsearch mapping code from different sources.
+ */
+interface MappingGeneratorInterface
+{
+    /**
+     * Generates the mapping body code.
+     *
+     * @param string $var Variable name to use in the generated code.
+     * @param array $data Optional data to use instead of the internal data.
+     * @return string The generated mapping code.
+     */
+    public function body(string $var = 'index', array $data = []): string;
+    
+    /**
+     * String representation of the generator output.
+     *
+     * @return string The generated mapping code.
+     */
+    public function __toString();
+}
