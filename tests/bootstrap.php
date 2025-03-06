b38cdb0ef4078b065ee6f88b69197dcf80f3d08d
Joao Zanon <jot@jot.com.br>
2025-03-06 14:09:42 -0300

    chore: atualiza configurau00e7u00f5es do projeto e dependu00eancias

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -11,3 +11,6 @@ date_default_timezone_set('America/Sao_Paulo');
 !defined('BASE_PATH') && define('BASE_PATH', dirname(__DIR__, 1));
 
 require BASE_PATH . '/vendor/autoload.php';
+
+! defined('SWOOLE_HOOK_FLAGS') && define('SWOOLE_HOOK_FLAGS', Hyperf\Engine\DefaultOption::hookFlags());
+
