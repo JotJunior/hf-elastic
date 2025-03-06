6312588e188542233ea7da865d51a69fa2b38776
Joao Zanon <jot@jot.com.br>
2025-03-06 14:12:20 -0300

    feat: atualiza ClientBuilder para integrar com nova arquitetura

--- src/ClientBuilder.php
+++ src/ClientBuilder.php
@@ -7,18 +7,27 @@ namespace Jot\HfElastic;
 use Elasticsearch\Client as ElasticsearchClient;
 use Hyperf\Contract\ConfigInterface;
 use Hyperf\Elasticsearch\ClientBuilderFactory;
+use Jot\HfElastic\Contracts\ClientFactoryInterface;
 use Psr\Container\ContainerInterface;
 
 /**
- * Service class for interacting with Elasticsearch through a configured client.
+ * Service class for creating and configuring Elasticsearch clients.
  */
-class ClientBuilder
+class ClientBuilder implements ClientFactoryInterface
 {
-
+    /**
+     * @var ClientBuilderFactory Factory for creating Elasticsearch client builders.
+     */
     private ClientBuilderFactory $clientBuilderFactory;
 
+    /**
+     * @var array Configuration for the Elasticsearch client.
+     */
     private array $config;
 
+    /**
+     * @param ContainerInterface $container The dependency injection container.
+     */
     public function __construct(ContainerInterface $container)
     {
         $this->clientBuilderFactory = $container->get(ClientBuilderFactory::class);
@@ -29,23 +38,26 @@ class ClientBuilder
         ]);
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function build(): ElasticsearchClient
     {
         $clientBuilder = $this->clientBuilderFactory->create();
         $clientBuilder->setHosts($this->config['hosts']);
+        
         if (!empty($this->config['username']) && !empty($this->config['password'])) {
             $clientBuilder->setBasicAuthentication($this->config['username'], $this->config['password']);
         }
+        
         if (!empty($this->config['api_key']) && !empty($this->config['api_id'])) {
             $clientBuilder->setApiKey($this->config['api_key'], $this->config['api_id']);
         }
+        
         if (!empty($this->config['ssl_verification'])) {
             $clientBuilder->setSSLVerification($this->config['ssl_verification']);
         }
-        if (!empty($this->config['ssl_verification'])) {
-            $clientBuilder->setSSLVerification($this->config['ssl_verification']);
-        }
+        
         return $clientBuilder->build();
     }
-
 }
