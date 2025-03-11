967e6339d1d1202f783b454b46898c3fa59f94c0
Joao Zanon <jot@jot.com.br>
2025-03-11 12:28:01 -0300

    test: add unit tests for BooleanType class

--- src/Migration/ElasticType/BooleanType.php
+++ src/Migration/ElasticType/BooleanType.php
@@ -8,5 +8,72 @@ class BooleanType extends AbstractField
 {
 
     public Type $type = Type::boolean;
-
+    
+    protected array $options = [
+        'boost' => null,
+        'doc_values' => null,
+        'index' => null,
+        'null_value' => null,
+        'store' => null,
+    ];
+    
+    /**
+     * Define o boost para o campo
+     * 
+     * @param float $value
+     * @return self
+     */
+    public function boost(float $value): self
+    {
+        $this->options['boost'] = $value;
+        return $this;
+    }
+    
+    /**
+     * Define se o campo deve ser armazenado em doc_values
+     * 
+     * @param bool $value
+     * @return self
+     */
+    public function docValues(bool $value): self
+    {
+        $this->options['doc_values'] = $value;
+        return $this;
+    }
+    
+    /**
+     * Define se o campo deve ser indexado
+     * 
+     * @param bool $value
+     * @return self
+     */
+    public function index(bool $value): self
+    {
+        $this->options['index'] = $value;
+        return $this;
+    }
+    
+    /**
+     * Define o valor a ser usado quando o campo é nulo
+     * 
+     * @param bool $value
+     * @return self
+     */
+    public function nullValue(bool $value): self
+    {
+        $this->options['null_value'] = $value;
+        return $this;
+    }
+    
+    /**
+     * Define se o campo deve ser armazenado
+     * 
+     * @param bool $value
+     * @return self
+     */
+    public function store(bool $value): self
+    {
+        $this->options['store'] = $value;
+        return $this;
+    }
 }
